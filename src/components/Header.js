b15a88251d071779945ecfad2ac24ff232635c6e
Josip Susic <josip.susic@hotmail.com>
2017-08-07 22:11:16 +0200

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Text>Albums</Text>
+    );
+  }
+}
+
+export default Header;
