a01e62f85a477feaef082b476c7bf9ddd67c5c2d
Josip Susic <josip.susic@hotmail.com>
2017-08-13 16:17:24 +0200

    header stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 class Header extends React.Component {
   render() {
+  	const { textStyle, viewStyle } = styles;
+  	var props = this.props; //?
+
     return (
-      <Text>Albums</Text>
+      <View style={viewStyle}>
+      	 <Text style={textStyle}>{props.headerText}</Text>
+      </View>
     );
   }
 }
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+	    borderColor: '#ddd',
+	    borderBottomWidth: 1,
+		shadowOffset: { width: 0, height: 20 },
+		shadowColor: '#000',
+		shadowOpacity: 0.2
+	},
+
+	textStyle: {
+		fontSize: 20
+	}
+};
 
 export default Header;
