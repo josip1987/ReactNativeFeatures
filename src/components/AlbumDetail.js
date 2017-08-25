1b091f093b952161a75357bd749cdd135bc12eb7
Josip Susic <josip.susic@hotmail.com>
2017-08-25 18:56:10 +0200

    basic layout

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => { // receive props object from parent
+					// or just (props)...
+const AlbumDetail = ({ albumProp }) => { // receive props object(s)
+								 // pass components as props
+								 // Card, CardSection...
+
+	// it's better to destructure
+	const { title, artist, thumbnail_image } = albumProp;
+	const { thumbnailStyle, headerContentStyle } = styles;
+
     return (
       <Card>
-          <Text>{props.albumProp.title}</Text>
+      	 <CardSection>
+      	 	<View>
+      	 	<Image 
+      	 	    style={thumbnailStyle} 
+      	 		source={{ uri: thumbnail_image }} 
+      	 	/>
+      	 	</View>
+      	 	<View style={headerContentStyle}>
+      	 		<Text>{title}</Text>
+      	 		<Text>{artist}</Text>
+      	 	</View>
+      	 </CardSection>
       </Card>
     );
+};
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	}
 };
 
 export default AlbumDetail;
