e21c676ea8277e60149bd2351b353a64ced5eeb8
Josip Susic <josip.susic@hotmail.com>
2017-08-26 18:58:32 +0200

    styling some components

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component {
-  render() {
+  render() { // flex on View goes because of the ScrollView
+             // expand component to fit entire content
     return (
-        <View>
+        <View style={{ flex: 1 }}> 
            <Header headerText={'Albums'} />
            <AlbumList />
         </View>
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,30 +2,48 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 					// or just (props)...
-const AlbumDetail = ({ albumProp }) => { // receive props object(s)
+const AlbumDetail = ({ albumProp }) => { 
+                 // receive props object(s)
 								 // pass components as props
 								 // Card, CardSection...
 
 	// it's better to destructure
-	const { title, artist, thumbnail_image } = albumProp;
-	const { thumbnailStyle, headerContentStyle } = styles;
+	const { title, artist, thumbnail_image, image } = albumProp;
+	const { 
+    thumbnailStyle, 
+    headerContentStyle, 
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle } = styles;
 
     return (
       <Card>
       	 <CardSection>
-      	 	<View>
-      	 	<Image 
-      	 	    style={thumbnailStyle} 
-      	 		source={{ uri: thumbnail_image }} 
-      	 	/>
-      	 	</View>
-      	 	<View style={headerContentStyle}>
-      	 		<Text>{title}</Text>
-      	 		<Text>{artist}</Text>
-      	 	</View>
+        	 	<View style={thumbnailContainerStyle}>
+        	 	<Image 
+        	 	    style={thumbnailStyle} 
+        	 		  source={{ uri: thumbnail_image }} 
+        	 	/>
+        	 	</View>
+        	 	<View style={headerContentStyle}>
+        	 		<Text style={headerTextStyle}>{title}</Text>
+        	 		<Text>{artist}</Text>
+        	 	</View>
       	 </CardSection>
+
+         <CardSection>
+            <Image 
+                style={imageStyle} 
+                source={{ uri: image }} 
+            />
+         </CardSection>
+         
+         <CardSection>
+            <Button />
+         </CardSection>
       </Card>
     );
 };
@@ -35,10 +53,24 @@ const styles = {
 		flexDirection: 'column',
 		justifyContent: 'space-around'
 	},
+  headerTextStyle: {
+    fontSize: 18
+  },
 	thumbnailStyle: {
 		height: 50,
 		width: 50
-	}
+	},
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null // must be null
+  }
 };
 
 export default AlbumDetail;
